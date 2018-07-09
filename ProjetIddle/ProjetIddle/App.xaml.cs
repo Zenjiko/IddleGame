a735d1a995873d9111ce576151ea2682b0d939f2
Zenjiko <dlzenjiko@gmail.com>
2018-07-09 17:55:46 +0200

    Money

--- ProjetIddle/ProjetIddle/App.xaml.cs
+++ ProjetIddle/ProjetIddle/App.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ProjetIddle.Views;
 
 [assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace ProjetIddle
@@ -11,7 +12,7 @@ namespace ProjetIddle
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new Money();
 		}
 
 		protected override void OnStart ()
---
+++ ProjetIddle/ProjetIddle/Views/Money.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetIddle.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Money : ContentPage
+	{
+        int valueMouche = 1;
+        int tmp;
+        public Money ()
+		{
+			InitializeComponent ();
+		}
+
+        private void buttonMouche_Clicked(object sender, EventArgs e)
+        {
+            var updateRate = 1000 / 30f; // 30Hz
+            double step = updateRate / (1 * 3 * 1000f);
+            Device.StartTimer(TimeSpan.FromMilliseconds(updateRate), () =>
+            {
+                if (progressMouche.Progress < 1)
+                {
+                    Device.BeginInvokeOnMainThread(() => progressMouche.Progress += step);
+                    return true;
+                } else if (progressMouche.Progress == 1)
+                {
+                    Device.BeginInvokeOnMainThread(() => progressMouche.Progress = 0);
+                    tmp += valueMouche;
+                    argentTotal.Text = tmp.ToString();
+                    return false;
+                }
+
+                return false;
+            });
+        }
+    }
+}
